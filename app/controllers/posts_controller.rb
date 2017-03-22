2388f08a3526b3a9b0b3d089a2c687f104cb8d75
“nataliedespain” <nataliedespainn@gmail.com>
2017-03-21 18:13:50 -0700

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all.reverse
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.create(post_params)
+		if @post.save
+			redirect_to posts_path
+		end
+	end
+
+	private
+
+		def post_params
+			params.require(:post).permit(:name, :title, :body)
+		end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  
+  get 'sessions/new'
+
+	root 'pages#index'
+
+	resources :posts do
+		resources :comments
+	end
+
+	resources :users
+
+	get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+end
