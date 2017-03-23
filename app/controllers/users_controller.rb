dd3de65650edfd4f0e0ef8e80fcfaee4c0c0ee94
“nataliedespain” <nataliedespainn@gmail.com>
2017-03-23 10:06:29 -0700

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-	def show
+	def index
+    @users = User.all
+  end
+
+  def show
     @user = User.find(params[:id])
   end
 
@@ -14,6 +18,15 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
     end
   end
 
